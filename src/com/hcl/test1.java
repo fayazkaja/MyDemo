e88169ff5deee8a108f73269bb7fddfe30836f8b
fayazkaja <fayazkaja@gmail.com>
2023-08-11 20:01:03 +0530

    Pushing to repository

---
+++ src/com/hcl/test1.java
@@ -0,0 +1,17 @@
+package com.hcl;
+
+public class test1 {
+
+	private void method1() {
+		System.out.println("Method1");
+	}
+	private void method2() {
+		System.out.println("Method2");
+	}
+	private void method3() {
+		System.out.println("Method3");
+	}
+	private void method4() {
+		System.out.println("Method4");
+	}
+}
